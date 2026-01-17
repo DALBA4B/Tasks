2ff35be8b5b0fa0910567e73e84b7759c59abd6b
DALBA4B <timagolud7@gmail.com>
2026-01-17 15:02:59 +0200

    исправил синию подсветку при нажатии на что угодно(ну пожалуйста щас)

--- service-worker.js
+++ service-worker.js
@@ -97,3 +97,10 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
+// Оптимизация для mobile - убираем задержку 300ms при touch
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
